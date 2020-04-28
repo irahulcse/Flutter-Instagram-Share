3014d5bd2b633dd308f249b0bcd559cca940444b
irahulcse <1rahulchandra1@gmail.com>
2020-04-28 22:01:19 +0530

    Automatic Commit 2020-04-28-22:01:19

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershare/widgets/header.dart';
 import 'package:fluttershare/widgets/progress.dart';
+
+final userRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -8,6 +11,21 @@ class Timeline extends StatefulWidget {
 }
 
 class _TimelineState extends State<Timeline> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    getUsers();
+    super.initState();
+  }
+
+  getUsers() {
+    userRef.getDocuments().then((QuerySnapshot snapshot) {
+      snapshot.documents.forEach((DocumentSnapshot doc) {
+        print(doc.data);
+      });
+    });
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
