be289cc253ea7c66fc86d76c6484b48277718458
irahulcse <1rahulchandra1@gmail.com>
2020-04-28 15:39:57 +0530

    Automatic Commit 2020-04-28-15:39:53

---
+++ lib/pages/home.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isAuth = false;
+  Widget buildAuthScreen() {
+    return Text('Authenticated');
+  }
+
+  Scaffold buildUnAuthScreen() {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Flutter Share',
+              style: TextStyle(
+                fontFamily: "Signatra",
+                fontSize: 90.0,
+                color: Colors.white,
+              ),
+            ),
+            GestureDetector(
+              child: Container( 
+                width: 260.0,
+                height: 60.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'assets/images/google_signin_button.png',
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isAuth ? buildAuthScreen() : buildUnAuthScreen();
+  }
+}
