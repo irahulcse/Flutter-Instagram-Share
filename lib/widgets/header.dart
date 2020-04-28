3b5488a8a56b3b809fe5248a499b2d4fc56072d8
irahulcse <1rahulchandra1@gmail.com>
2020-04-28 21:33:35 +0530

    Automatic Commit 2020-04-28-21:33:35

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
-header() {
-  return Text("header");
+AppBar header(context, {bool isAppTitle = false, String titleText}) {
+  return AppBar(
+    title: Text(
+      isAppTitle ? 'FriendChat' : titleText,
+      style: TextStyle(
+        color: Colors.white,
+        fontFamily: isAppTitle ? "Signatra": " ",
+        fontSize: isAppTitle ? 50.0 :20.0,
+      ),
+    ),
+    centerTitle: true,
+    backgroundColor: Theme.of(context).accentColor,
+  );
 }
