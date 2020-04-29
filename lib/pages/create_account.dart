0034132684b32009d3b838f1008a4cf8dc74b705
irahulcse <1rahulchandra1@gmail.com>
2020-04-29 18:01:52 +0530

    Automatic Commit 2020-04-29-18:01:52

--- lib/pages/create_account.dart
+++ lib/pages/create_account.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:fluttershare/widgets/header.dart';
 
@@ -9,15 +11,28 @@ class CreateAccount extends StatefulWidget {
 class _CreateAccountState extends State<CreateAccount> {
   final _formKey = GlobalKey<FormState>();
   String username;
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   submit() {
-    _formKey.currentState.save();
-    Navigator.pop(context, username);
+    final form = _formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      _formKey.currentState.save();
+      SnackBar snackbar = SnackBar(content: Text("Welcome $username! "));
+      _scaffoldKey.currentState.showSnackBar(snackbar);
+      Timer(
+          Duration(
+            seconds: 2,
+          ), () {
+        Navigator.pop(context, username);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext parentContext) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: header(context, titleText: "Set up your profile"),
       body: ListView(
         children: <Widget>[
@@ -38,7 +53,17 @@ class _CreateAccountState extends State<CreateAccount> {
                   child: Container(
                     child: Form(
                       key: _formKey,
+                      autovalidate: true,
                       child: TextFormField(
+                        validator: (val) {
+                          if (val.trim().length < 3 || val.isEmpty) {
+                            return "Username too short";
+                          } else if (val.trim().length > 12) {
+                            return "Username too long";
+                          } else {
+                            return null;
+                          }
+                        },
                         onSaved: (val) => username = val,
                         decoration: InputDecoration(
                           border: OutlineInputBorder(),
