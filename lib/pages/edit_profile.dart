0bdb64674f4a0281f22e08a7caa7e322002eeda5
irahulcse <1rahulchandra1@gmail.com>
2020-05-01 13:30:14 +0530

    Automatic Commit 2020-05-01-13:30:14

--- lib/pages/edit_profile.dart
+++ lib/pages/edit_profile.dart
@@ -7,35 +7,34 @@ import 'package:fluttershare/widgets/progress.dart';
 class EditProfile extends StatefulWidget {
   final String currentUserId;
 
-  EditProfile({this.currentUserId};)
+  EditProfile({this.currentUserId});
   @override
   _EditProfileState createState() => _EditProfileState();
 }
 
 class _EditProfileState extends State<EditProfile> {
-  bool isLoading= false;
+  bool isLoading = false;
   User user;
-  TextEditingController displayNameController= TextEditingController();
-  TextEditingController bioController= TextEditingController();           
+  TextEditingController displayNameController = TextEditingController();
+  TextEditingController bioController = TextEditingController();
   @override
-  
   void initState() {
     // TODO: implement initState
     super.initState();
     getUser();
   }
 
-  getUser() async{ 
-   setState(() {
-     isLoading=true;
-   });
-    DocumentSnapshot doc= await usersRef.document(widget.currentUserId).get();
-      user =User.fromDocument(doc);
-    displayNameController.text= user.displayName;
-    bioController.text=user.bio;
+  getUser() async {
     setState(() {
-     isLoading=false;
-   });
+      isLoading = true;
+    });
+    DocumentSnapshot doc = await usersRef.document(widget.currentUserId).get();
+    user = User.fromDocument(doc);
+    displayNameController.text = user.displayName;
+    bioController.text = user.bio;
+    setState(() {
+      isLoading = false;
+    });
   }
 
   @override
@@ -45,26 +44,39 @@ class _EditProfileState extends State<EditProfile> {
         backgroundColor: Colors.white,
         title: Text(
           'Editing Profile',
-          style : TextStyle(
+          style: TextStyle(
             color: Colors.white,
           ),
         ),
         actions: <Widget>[
-          
-          IconButton(icon: Icon(Icons.done, size: 30.0, color: Colors.green,), onPressed: ()=> Navigator.pop(context),),
+          IconButton(
+            icon: Icon(
+              Icons.done,
+              size: 30.0,
+              color: Colors.green,
+            ),
+            onPressed: () => Navigator.pop(context),
+          ),
         ],
       ),
-      body: isLoading? circularProgress(): ListView(
-        children: <Widget>[
-          Container(
-            child: Column(
+      body: isLoading
+          ? circularProgress()
+          : ListView(
               children: <Widget>[
-                pad
+                Container(
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: 20.0,
+                          bottom: 20.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
-          )
-        ],
-      ),
     );
   }
 }
