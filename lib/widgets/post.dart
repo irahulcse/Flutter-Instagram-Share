80d6eb8ff9f8e1cdea2bbb59edb7cb5f9fbfba00
irahulcse <1rahulchandra1@gmail.com>
2020-05-03 12:08:42 +0530

    Automatic Commit 2020-05-03-12:08:42

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class Post extends StatefulWidget {
   @override
   _PostState createState() => _PostState();
+  
 }
 
 class _PostState extends State<Post> {
